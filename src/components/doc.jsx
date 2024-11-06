b0b833aa0cf401e78e0095e6fda338fbc93db11f
DrZiMo <zuhaybhamar@gmail.com>
2024-11-06 14:31:50 +0300

    fixed some errors

--- src/components/doc.jsx
+++ src/components/doc.jsx
@@ -4,10 +4,7 @@ const Doc = ({ fileType, fileName, courseName, shift, classIdentify }) => {
   return (
     <div className="doc p-4 shadow-box rounded-md flex flex-col gap-3">
       <div className="cover-part h-20 mx-auto">
-        <img
-          src={`../public/assets/${fileType.toLowerCase()}.png`}
-          className="h-full"
-        />
+        <img src={`/assets/${fileType.toLowerCase()}.png`} className="h-full" />
       </div>
       <div className="doc-info">
         <div className="doc-name font-bold text-xl">{fileName}</div>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/student-docs/Front-end'
+    base: '/studentdocs'
 });
