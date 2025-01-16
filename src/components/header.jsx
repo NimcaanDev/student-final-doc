94cd31ed2e3c5ba8a84dbd546cdc476b21c2ae59
DrZiMo <zuhaybhamar@gmail.com>
2025-01-16 23:17:18 +0300

    changed some colors

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,12 +11,12 @@ const Header = () => {
   }
 
   return (
-    <div className="header bg-blue-700 p-4 px-3 md:px-6 flex justify-between items-center sticky top-0 z-50 md:static">
+    <div className="header shadow-md bg-blue-700 p-4 px-3 md:px-6 flex justify-between items-center sticky top-0 z-50 md:static">
       <div className="logo font-konit text-white text-3xl">
         StudentDocs
       </div>
       <div className="right-part flex gap-4 items-center">
-        <div className="upload border-2 border-white text-white py-1 px-5 transition hover:bg-white rounded-md hover:text-black cursor-pointer">
+        <div className="upload border-2 border-white text-white py-1 px-5 transition hover:bg-white  rounded-md hover:text-blue-700 cursor-pointer">
           <Link to='/studentdocs/upload'>Upload</Link>
         </div>
         <div>
