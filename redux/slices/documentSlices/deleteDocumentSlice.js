f7c5a3688804f8dd9b01fa9cb4d7498b1e09770d
DrZiMo <zuhaybhamar@gmail.com>
2025-02-01 22:25:01 +0300

    fixed the deleteDocumentFn

--- redux/slices/documentSlices/deleteDocumentSlice.js
+++ redux/slices/documentSlices/deleteDocumentSlice.js
@@ -15,7 +15,7 @@ export const deleteDocumentFn = createAsyncThunk(
         const stateData = getState()
         const { token } = stateData?.user?.data
         try {
-            const res = await axios.delete(`${baseUrl}documents/delete`, id, {
+            const res = await axios.delete(`${baseUrl}documents/delete/${id}`, {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 },
