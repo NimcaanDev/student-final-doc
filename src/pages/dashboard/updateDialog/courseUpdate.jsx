c40f7e1598d37543f94552508b937ff271241568
DrZiMo <zuhaybhamar@gmail.com>
2025-02-02 12:35:45 +0300

    changed the text of the update course

--- src/pages/dashboard/updateDialog/courseUpdate.jsx
+++ src/pages/dashboard/updateDialog/courseUpdate.jsx
@@ -95,10 +95,10 @@ const UpdateCourse = ({ course_id }) => {
                 <Dialog.Overlay className="fixed inset-0 bg-blackA6 data-[state=open]:animate-overlayShow" />
                 <Dialog.Content className="fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] max-w-[500px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-gray1 p-[25px] shadow-md focus:outline-none data-[state=open]:animate-contentShow bg-gray-800 text-white">
                     <Dialog.Title className="m-0 text-[17px] font-medium text-mauve12">
-                        Update Class
+                        Update Course
                     </Dialog.Title>
                     <Dialog.Description className="mb-5 mt-2.5 text-[15px] leading-normal text-mauve11">
-                        Make changes to the class here. Click save when you're done.
+                        Make changes to the course here. Click save when you're done.
                     </Dialog.Description>
                     <form onSubmit={formik.handleSubmit}>
                         <fieldset className="mb-[15px] flex items-center gap-5">
@@ -137,7 +137,7 @@ const UpdateCourse = ({ course_id }) => {
                         <fieldset>
                             <p className="text-sm font-bold text-red-500">
                                 {formik.touched.semester && formik.errors.semester}
-                            </p>
+                            </p>j
                         </fieldset>
                         <div className="mt-[25px] flex justify-end">
                             <button type="submit" className="inline-flex h-[35px] items-center justify-center rounded bg-green-700 px-[15px] font-medium leading-none text-green-200 outline-none outline-offset-1 hover:bg-green-900 focus-visible:outline-2 focus-visible:outline-green-200 select-none">
